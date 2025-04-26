ea83fe88f71bbd6503d16f3c52fe06f944e54052
Yevhen Oliinyk <oleynik.zh10@gmail.com>
2025-04-26 21:25:58 +0300

    Use path in the update_full_url function.

--- packages/backend/supabase/migrations/20250426181440_endpoint_path.sql
+++ packages/backend/supabase/migrations/20250426181440_endpoint_path.sql
@@ -1,2 +1,16 @@
 alter table public.endpoints
 add column path text;
+
+create or replace function update_full_url(p_service_id integer)
+    returns void
+    security definer
+    language plpgsql
+as
+$$
+begin
+    update public.endpoints e
+    set full_url = concat(s.base_url, e.path)
+    from public.services s
+    where s.id = p_service_id and e.service_id = s.id;
+end;
+$$;
