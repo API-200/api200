0c665852cb25e96a8598092b9da7599a1453e040
maxim.budnik <maxim.budnyk@gmail.com>
2025-04-28 22:19:52 +0300

    import collections

--- packages/dashboard/src/utils/data/apiServices.ts
+++ packages/dashboard/src/utils/data/apiServices.ts
@@ -5,4 +5,58 @@ export  const apiServices = [
         image: "https://res.cloudinary.com/postman/image/upload/t_team_logo/v1713587390/team/44e00e03a4986050c602a99b246289edf3f77059f8448d415c716eb1fd9132c4.jpg",
         configUrl: "https://api-200.github.io/service-configs/X%20API%20v2.postman_collection.json",
     },
+    {
+        name: "Slack Web API",
+        description: "",
+        image: "https://res.cloudinary.com/postman/image/upload/t_team_logo/v1629872709/team/e20e2ef7a54bb2223e5cdb37fc8b0198649b405cf1434468c40733f8ea5f6daf",
+        configUrl: "https://api-200.github.io/service-configs/Slack%20Web%20API.postman_collection.json",
+    },
+    {
+        name: "Notion API",
+        description: "A collection of requests to help you get started with Notion API's public beta.",
+        image: "https://res.cloudinary.com/postman/image/upload/t_team_logo/v1629871571/team/5e4b440c0964a0f4eeffc6ce3f8b3b2869ef1fa52badc957844bf302ae91b103",
+        configUrl: "https://api-200.github.io/service-configs/Notion%20API.postman_collection.json",
+    },
+    {
+        name: "Pipedrive API v1",
+        description: "The official Pipedrive API that enables developing apps and integrations on the Pipedrive platform.",
+        image: "https://res.cloudinary.com/postman/image/upload/t_team_logo/v1686310308/team/cad7879aadf6070a54d8849e12fb3c07555707d8ebf7bc26f2de892037e2bcb6.png",
+        configUrl: "https://api-200.github.io/service-configs/Pipedrive%20API%20v1.postman_collection.json",
+    },
+    {
+        name: "Instagram API",
+        description: "Official Instagram API collection.",
+        image: "https://res.cloudinary.com/postman/image/upload/t_team_logo/v1635557679/team/b9724884cd02ea2f032eab63b55ac6ea4a2869516a26292bc1368d03c60f2054.png",
+        configUrl: "https://api-200.github.io/service-configs/Instagram%20API.postman_collection.json",
+    },
+    {
+        name: "Datadog API",
+        description: "This is the latest collection for the Datadog REST API.",
+        image: "https://res.cloudinary.com/postman/image/upload/t_team_logo/v1629887344/team/cce3460089092278711346c5a8da9d3b85714361377fc9875a860df8fa6102a3.jpg",
+        configUrl: "https://api-200.github.io/service-configs/Datadog%20API%20Collection.postman_collection.json",
+    },
+    {
+        name: "Brevo API",
+        description: "This collection provides all the Brevo API endpoints and documentation for all our verticals: Transactional, Marketing, eCommerce and more.",
+        image: "https://res.cloudinary.com/postman/image/upload/t_team_logo/v1683205575/team/668633841ffb2ef18489566f55e6610ffffd9cdbb228a959b37eb097c97eb634.png",
+        configUrl: "https://api-200.github.io/service-configs/Brevo.postman_collection.json",
+    },
+    {
+        name: "Booking API",
+        description: "Booking API is designed to book hotels in real time as fast as in two steps. It covers the complete booking process.",
+        image: "https://res.cloudinary.com/postman/image/upload/t_team_logo/v1629889040/team/072fec6f8f33960116253f4da1b74f2dcfa03dd455eab1f9746d99acc804482c",
+        configUrl: "https://api-200.github.io/service-configs/BookingAPI.postman_collection.json",
+    },
+    {
+        name: "Discord API",
+        description: "HTTP-based API to interact with Discord",
+        image: "https://res.cloudinary.com/postman/image/upload/t_team_logo/v1673463107/team/da5ee2793e4028c5d94745e669be3abf34b8d07f54498288ba095e1dee0c67c2.png",
+        configUrl: "https://api-200.github.io/service-configs/Discord%20REST%20API.postman_collection.json",
+    },
+    {
+        name: "DeepL API",
+        description: "This is the up to date collection of endpoints for DeepL API",
+        image: "https://res.cloudinary.com/postman/image/upload/t_team_logo/v1687949678/team/110eab428e74995cd01ad841e3d501cf99b716766d6e0329435364b733910a27.png",
+        configUrl: "https://api-200.github.io/service-configs/DeepL%20API.postman_collection.json",
+    }
 ]
