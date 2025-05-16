d264be3141ca97a9cfb91f786d256e84459fb506
Yevhen Oliinyk <oleynik.zh10@gmail.com>
2025-05-16 21:31:47 +0300

    Update usage bar.

--- packages/dashboard/src/utils/constants.ts
+++ packages/dashboard/src/utils/constants.ts
@@ -2,3 +2,12 @@
 export const DAY_MS = 24 * 60 * 60 * 1000;
 
 export const EXTERNAL_ENDPOINTS_SOURCES = ['openapi', 'postman'];
+
+export const PLANS = {
+    BASIC: {
+        REQUESTS_PER_MONTH: 100
+    },
+    PRO: {
+        REQUESTS_PER_MONTH: 10000
+    }
+} as const;
